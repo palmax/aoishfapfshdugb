78c5c2f38f85bf47ad2b3603ae63a5c8ccce0ad6
palmax <palmax@39f93949-1e30-0410-a901-09a4849b322f>
2007-05-15 20:35:10 +0000

    Corregidos unos churros enormes

--- A_entregar/10.sql
+++ A_entregar/10.sql
@@ -1,13 +1,16 @@
 SELECT VACA.n_crotal,ANIMAL.nombre
 FROM  VACA, ANIMAL
-WHERE VACA.n_crotal NOT IN(SELECT INSEMINA.n_crotal
-                       FROM INSEMINA,ANIMAL,PARE
-	  	       WHERE INSEMINA.Lote IN(SELECT SUMINISTRA.Lote
-						       FROM SUMINISTRA
-						       WHERE SUMINISTRA.codP IN (SELECT PROVEEDOR.codP 
-										 FROM PROVEEDOR 
-										 WHERE PROVEEDOR.nombre='X'))
-                        AND ANIMAL.fecha_nacimiento BETWEEN INSEMINA.Fecha + 276 AND INSEMINA.Fecha + 290
-			AND PARE.n_crotal_hijo=ANIMAL.n_crotal AND PARE.n_crotal_madre=INSEMINA.n_crotal
-                        )
-	  AND ANIMAL.n_crotal = VACA.n_crotal;
+WHERE VACA.n_crotal 
+      NOT IN(SELECT INSEMINA.n_crotal
+             FROM INSEMINA,ANIMAL,PARE
+             WHERE INSEMINA.Lote
+                   IN(SELECT SUMINISTRA.Lote
+                      FROM SUMINISTRA
+                      WHERE SUMINISTRA.codP
+                            IN (SELECT PROVEEDOR.codP 
+                                FROM PROVEEDOR 
+                                WHERE PROVEEDOR.nombre='X'))
+                   AND ANIMAL.fecha_nacimiento BETWEEN INSEMINA.Fecha + 276 AND INSEMINA.Fecha + 290
+                   AND PARE.n_crotal_hijo=ANIMAL.n_crotal
+                   AND PARE.n_crotal_madre=INSEMINA.n_crotal)
+      AND ANIMAL.n_crotal = VACA.n_crotal;
