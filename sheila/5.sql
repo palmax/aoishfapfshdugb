d6884448e43b9d3a9b7e7def4f6882744bcab6cb
sheilazuniga <sheilazuniga@39f93949-1e30-0410-a901-09a4849b322f>
2007-05-16 08:42:08 +0000

    para la memoria

---
+++ sheila/5.sql
@@ -0,0 +1,7 @@
+SELECT ANIMAL.n_crotal, ANIMAL.nombre AS Nombre_Vaca,RAZA.Nombre AS Nombre_Raza,
+	ANIMAL.tipo,RAZA.Nombre,Insemina.Fecha AS Fecha_Inseminacion
+FROM ANIMAL,pertenece,RAZA,Insemina
+WHERE A NIMAL.n_crotal=pertenece.n_crotal and pertenece.cod=RAZA.Cod and 
+      Insemina.n_crotal = ANIMAL.n_crotal and
+      (DATE_ADD(insemina.Fecha,INTERVAL 290 DAY) < '2007-09-30' and
+       DATE_ADD(insemina.Fecha, INTERVAL 276 Day)>'2007-09-01');
--- sheila/Sheila.sql
+++ sheila/Sheila.sql
@@ -8,22 +8,55 @@ WHERE SEMENTAL.n_crotal IN (SELECT PERTENECE.n_crotal
 				     WHERE RAZA.nombre = 'Alistana'));
 
 SELECT VACA.n_crotal, ANIMAL.nombre, RAZA.nombre
-FROM VACA, RAZA, PERTENECE, ANIMAL
+FROM VACA,RAZA,PERTENECE,ANIMAL
 WHERE ANIMAL.n_crotal = VACA.n_crotal AND
-	VACA.n_crotal=PERTENECE.n_crotal AND RAZA.cod=PERTENECE.cod AND NOT EXISTS(SELECT PARE.n_crotal_madre
+      VACA.n_crotal=PERTENECE.n_crotal AND
+      RAZA.cod=PERTENECE.cod AND NOT EXISTS(SELECT PARE.n_crotal_madre
 							   FROM PARE
 							   WHERE PARE.n_crotal_madre = VACA.n_crotal);
 
 
 
-SELECT VACA.n_crotal, COUNT(PARE.n_crotal_madre)
+SELECT VACA.n_crotal, COUNT(PARE.n_crotal_madre)AS Num_Hijos
 FROM VACA, PARE
 WHERE VACA.n_crotal=PARE.n_crotal_madre
 GROUP BY PARE.n_crotal_madre;
 
 
 
-SELECT RAZA.nombre, COUNT(RAZA.cod)
+SELECT RAZA.nombre, COUNT(RAZA.cod) AS Num_Sementales 
 FROM SEMENTAL, RAZA, PERTENECE
 WHERE SEMENTAL.n_crotal=PERTENECE.n_crotal AND RAZA.cod=PERTENECE.cod
-GROUP BY RAZA.cod
+GROUP BY RAZA.cod;
+
+
+SELECT VACA.n_crotal,ANIMAL.nombre
+FROM  VACA, ANIMAL
+WHERE VACA.n_crotal NOT IN(SELECT INSEMINA.n_crotal
+                       FROM INSEMINA,ANIMAL,PARE
+	  	       WHERE INSEMINA.Lote IN(SELECT SUMINISTRA.Lote
+						       FROM SUMINISTRA
+						       WHERE SUMINISTRA.codP IN (SELECT PROVEEDOR.codP 
+										 FROM PROVEEDOR 
+										 WHERE PROVEEDOR.nombre='X'))
+                        AND ANIMAL.fecha_nacimiento BETWEEN INSEMINA.Fecha + 276 AND INSEMINA.Fecha + 290
+			AND PARE.n_crotal_hijo=ANIMAL.n_crotal AND PARE.n_crotal_madre=INSEMINA.n_crotal
+                        )
+	  AND ANIMAL.n_crotal = VACA.n_crotal ;
+
+  
+SELECT ANIMAL.n_crotal, ANIMAL.nombre AS Nombre_Vaca,RAZA.Nombre AS Nombre_Raza,
+	ANIMAL.tipo,RAZA.Nombre,Insemina.Fecha AS Fecha_Inseminacion
+FROM ANIMAL,pertenece,RAZA,Insemina
+WHERE ANIMAL.n_crotal=pertenece.n_crotal and pertenece.cod=RAZA.Cod and 
+      Insemina.n_crotal = ANIMAL.n_crotal and (DATE_ADD(insemina.Fecha,INTERVAL 290 DAY) < '2007-09-30' and DATE_ADD(insemina.Fecha, INTERVAL 276 Day)>'2007-09-01');
+
+CREATE VIEW VACA_RAZA AS 
+SELECT ANIMAL.nombre, VACA.n_crotal, RAZA.nombre
+FROM ANIMAL,VACA, pertenece, RAZA
+WHERE VACA.n_crotal = ANIMAL.n_crotal AND pertenece.n_crotal= vaca.n_crotal and pertenece.Cod=raza.Cod;
+
+CREATE VIEW SEMENTAL_RAZA AS 
+SELECT ANIMAL.nombre AS animalito, SEMENTAL.n_crotal,RAZA.nombre as raza
+FROM ANIMAL,SEMENTAL,pertenece,RAZA
+WHERE SEMENTAL.n_crotal = ANIMAL.n_crotal AND SEMENTAL.n_crotal= pertenece.n_crotal AND pertenece.Cod=raza.cod;
